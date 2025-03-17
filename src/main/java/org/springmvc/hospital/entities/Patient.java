a85fba29e12f8172853f0f9d6748df3f60d57d69
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-17 13:32:55 +0000

    create the entity Patient

---
+++ src/main/java/org/springmvc/hospital/entities/Patient.java
@@ -0,0 +1,21 @@
+package org.springmvc.hospital.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Patient {
+    @Id @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private Date dateNaissance;
+    private boolean malade ;
+    private int score ;
+}
