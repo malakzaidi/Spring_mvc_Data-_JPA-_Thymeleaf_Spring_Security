5179e09767dd2f217d8a82791f17ba67ec922c06
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-17 18:41:36 +0000

    add the edit option

--- src/main/java/org/springmvc/hospital/web/PatientController.java
+++ src/main/java/org/springmvc/hospital/web/PatientController.java
@@ -51,7 +51,14 @@ public class PatientController {
     public String save(Model model, @Valid Patient patient, BindingResult bindingResult){
         if(bindingResult.hasErrors()) return "formPatients";
         patientRepository.save(patient);
-        return "redirect:/formPatients";
+        return "redirect:/index";
+    }
+    @GetMapping("/editPatient")
+    public String editPatient (Model model, Long id){
+        Patient patient = patientRepository.findById(id).orElse(null);
+        if(patient == null) throw new RuntimeException("Patient not found");
+        model.addAttribute("patient",patient);
+        return "editPatients";
     }
 
 }
