45df898df36c1757fa4e671bcfa76dd296a1bd8c
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-17 14:35:18 +0000

    use the annotation RequestParam in case the parameter given is not named page + use the method getTotalpages of the model

--- src/main/java/org/springmvc/hospital/web/PatientController.java
+++ src/main/java/org/springmvc/hospital/web/PatientController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springmvc.hospital.entities.Patient;
 import org.springmvc.hospital.repositories.PatientRepository;
 
@@ -17,9 +18,10 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model, int page, int size){
+    public String index(Model model, @RequestParam (name= "page",defaultValue = "0") int page ,@RequestParam (name ="size",defaultValue = "4") int size){
         Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
         model.addAttribute("patients",pagePatients.getContent());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
         return "patients";
     }
 }
