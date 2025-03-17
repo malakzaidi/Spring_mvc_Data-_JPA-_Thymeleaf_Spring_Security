14b090b3d1e744accda281153dbed857b168265f
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-17 15:07:57 +0000

    do the research by keyword using the annotation Pageable in the repository to return a page + add a param keyword

--- src/main/java/org/springmvc/hospital/repositories/PatientRepository.java
+++ src/main/java/org/springmvc/hospital/repositories/PatientRepository.java
@@ -1,8 +1,12 @@
 package org.springmvc.hospital.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springmvc.hospital.entities.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Page<Patient> findByNomContains(String keyword, Pageable pageable);
 
 }
