3037959308763695776d724dd06f5d0c84197c1d
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-24 14:47:40 +0000

    specify the users allowed to authentify in the application using PasswordEncoder to encode the passwords

--- src/main/java/org/springmvc/hospital/HospitalApplication.java
+++ src/main/java/org/springmvc/hospital/HospitalApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springmvc.hospital.entities.Patient;
 import org.springmvc.hospital.repositories.PatientRepository;
 
@@ -29,5 +31,9 @@ public class HospitalApplication  {
 
         };
 
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/org/springmvc/hospital/security/SecurityConfig.java
+++ src/main/java/org/springmvc/hospital/security/SecurityConfig.java
@@ -1,14 +1,32 @@
 package org.springmvc.hospital.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
  public class  SecurityConfig  {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin").password(passwordEncoder.encode("12")).roles("USER","ADMIN").build(),
+                User.withUsername("user1").password(passwordEncoder.encode("12")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode("12")).roles("USER").build()
+
+        );
+    }
+
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
