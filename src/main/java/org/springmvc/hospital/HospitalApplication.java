1828987b605574f2c65c06ee91ab03f44f19197f
Malak Zaidi <malakzaidi815@gmail.com>
2025-04-13 19:26:47 +0000

    create new users and affect roles to users

--- src/main/java/org/springmvc/hospital/HospitalApplication.java
+++ src/main/java/org/springmvc/hospital/HospitalApplication.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springmvc.hospital.entities.Patient;
 import org.springmvc.hospital.repositories.PatientRepository;
+import org.springmvc.hospital.security.service.AccountService;
 
 import java.util.Date;
 
@@ -60,5 +61,21 @@ public class HospitalApplication  {
             }
         };
     };
+    @Bean
+    CommandLineRunner commandLineRunnerUserDetails(AccountService accountService) {
+        return args -> {
+            accountService.addNewRole("USER");
+            accountService.addNewRole("ADMIN");
+            accountService.addNewUser("user1","1234","user1@gmail.com","1234");
+            accountService.addNewUser("user2","1234","user2@gmail.com","1234");
+            accountService.addNewUser("admin","1234","admin@gmail.com","1234");
+
+            accountService.addRoleToAppUser("ADMIN","admin");
+            accountService.addRoleToAppUser("USER","admin");
+            accountService.addRoleToAppUser("USER","user1");
+            accountService.addRoleToAppUser("USER","user2");
+
+        };
+    }
 }
 
